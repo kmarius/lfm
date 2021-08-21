6aba5027f3cfd169c2fee2be2fb6e2cbfcbc296e
kmarius <froase@gmail.com>
2021-08-21 22:36:03 +0200

    chore: add some docs

--- src/lualfm.h
+++ src/lualfm.h
@@ -22,12 +22,13 @@ bool lua_load_file(lua_State *L, app_t *app, const char *path);
 void lua_handle_key(lua_State *L, app_t *app, ncinput *in);
 
 /*
- * Execute an expr.
+ * Execute an expr, which is either a chunk of lua code or a registered command
+ * (with arguments) as if typed in the command line.
  */
 void lua_exec_expr(lua_State *L, app_t *app, const char *cmd);
 
 /*
- * Run hooks.
+ * Run hooks. Hook need to be registered in core.lua.
  */
 void lua_run_hook(lua_State *L, const char *hook);
 
