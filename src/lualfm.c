b4101e0e4d49b019567769b00485361d22532cc5
kmarius <froase@gmail.com>
2021-10-31 10:26:39 +0100

    bug: remove highlight on escape

--- src/lualfm.c
+++ src/lualfm.c
@@ -180,6 +180,7 @@ void lua_handle_key(lua_State *L, app_t *app, ncinput *in)
 		if (key == 27) {
 			cur = NULL;
 			ui_cmd_clear(&app->ui);
+			ui_search_nohighlight(&app->ui);
 			fm_selection_visual_stop(&app->fm);
 			fm_selection_clear(&app->fm);
 			fm_load_clear(&app->fm);
