c87a4de3ba0f298f7bc77a0e3670cc9f5ca82c0a
kmarius <froase@gmail.com>
2021-11-12 17:18:12 +0100

    chore: document app.h

--- src/app.h
+++ src/app.h
@@ -16,20 +16,38 @@ typedef struct app_t {
 } app_t;
 
 /*
- * Set input timout. Key input will be ignored for the next LEN ms.
- * */
+ * Set input timout. Key input will be ignored for the next `duration` ms.
+ */
 void timeout_set(int duration);
 
+/*
+ * Initialize ui, fm and the lua_State.
+ */
 void app_init(app_t *app);
 
+/*
+ * Start the main event loop.
+ */
 void app_run(app_t *app);
 
+/*
+ * Stop the main event loop.
+ */
 void app_quit(app_t *app);
 
+/*
+ * Free all recources i.e. ui, fm and the lua_State.
+ */
 void app_deinit(app_t *app);
 
+/*
+ * Print a message in the UI. `printf` formatting applies.
+ */
 void print(const char *format, ...);
 
+/*
+ * Print an error in the UI. `printf` formatting applies.
+ */
 void error(const char *format, ...);
 
 #endif /* APP_H */
