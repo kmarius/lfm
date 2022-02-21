5f1b12e7d5401d5b1bf0aee18c9c75c70d6ddf44
kmarius <froase@gmail.com>
2022-02-21 16:39:40 +0100

    [filter]: specify filter options with |

--- src/filter.c
+++ src/filter.c
@@ -7,24 +7,54 @@
 #include "util.h"
 #include "log.h"
 
-#define FILTER_INITIAL_CAPACITY 4
+#define FILTER_INITIAL_CAPACITY 2
+
+/* TODO: test performance on large directories and check out if we can incrementally
+ * build a new filter from the previous one or vice-versa (on 2022-02-21) */
+
+#define T Filter
 
 struct Subfilter;
 
 typedef struct Filter {
 	char *string;
-	struct Subfilter *filters;
 	uint16_t length;
 	uint16_t capacity;
+	struct Subfilter *filters;
 } Filter;
 
-struct Subfilter {
+struct FilterAtom {
 	bool negate;
-	char *filter;
+	char *string;
+};
+
+struct Subfilter {
+	uint16_t length;
+	uint16_t capacity;
+	struct FilterAtom *atoms;
 };
 
 
-#define T Filter
+static void subfilter_init(struct Subfilter *s, char *filter)
+{
+	s->length = 0;
+	s->capacity = FILTER_INITIAL_CAPACITY;
+	s->atoms = malloc(s->capacity * sizeof(struct FilterAtom));
+
+	char *ptr;
+	for (char *tok = strtok_r(filter, "|", &ptr);
+			tok != NULL;
+			tok = strtok_r(NULL, "|", &ptr)) {
+		if (s->capacity == s->length) {
+			s->capacity *= 2;
+			s->atoms = realloc(s->atoms, s->capacity * sizeof(struct Subfilter));
+		}
+		s->atoms[s->length].negate = tok[0] == '!';
+		if (s->atoms[s->length].negate)
+			tok++;
+		s->atoms[s->length++].string = strdup(tok);
+	}
+}
 
 
 T *filter_create(const char *filter)
@@ -36,17 +66,14 @@ T *filter_create(const char *filter)
 	t->string = strdup(filter);
 
 	char *buf = strdup(filter);
-	char *tok = strtok(buf, " ");
-	for (; tok != NULL; tok = strtok(NULL, " ")) {
+	for (char *tok = strtok(buf, " ");
+			tok != NULL;
+			tok = strtok(NULL, " ")) {
 		if (t->length == t->capacity) {
 			t->capacity *= 2;
 			t->filters = realloc(t->filters, t->capacity * sizeof(struct Subfilter));
 		}
-		t->filters[t->length].negate = tok[0] == '!';
-		if (t->filters[t->length].negate)
-			tok++;
-		t->filters[t->length].filter = strdup(tok);
-		t->length++;
+		subfilter_init(&t->filters[t->length++], tok);
 	}
 
 	free(buf);
@@ -58,6 +85,11 @@ void filter_destroy(T *t)
 {
 	if (!t)
 		return;
+	for (uint16_t i = 0; i < t->length; i++) {
+		for (uint16_t j = 0; j < t->filters[i].length; j++)
+			free(t->filters[i].atoms[j].string);
+		free(t->filters[i].atoms);
+	}
 	free(t->string);
 	free(t->filters);
 	free(t);
@@ -74,8 +106,10 @@ const char *filter_string(T *t)
 
 static inline bool match(struct Subfilter *s, const char *str)
 {
-
-	return (strcasestr(str, s->filter) != NULL) != s->negate;
+	for (uint16_t i = 0; i < s->length; i++)
+		if ((strcasestr(str, s->atoms[i].string) != NULL) != s->atoms[i].negate)
+			return true;
+	return false;
 }
 
 
