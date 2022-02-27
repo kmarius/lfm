221b4b5d6c34b0aebd6c605db7429032178188e9
kmarius <froase@gmail.com>
2022-02-27 21:02:18 +0100

    move tokenizer back into its own source

---
+++ src/tokenize.c
@@ -0,0 +1,59 @@
+#include <stdlib.h> // NULL
+
+#include "tokenize.h"
+
+char *tokenize(const char *str, char *buf, const char **pos_str, char **pos_buf)
+{
+	char c;
+	if (str) {
+		*pos_str = str;
+		*pos_buf = buf;
+	}
+
+	while ((c = **pos_str) && c == ' ')
+		(*pos_str)++;
+
+	if (!c)
+		return NULL;
+
+	char *ret = *pos_buf;
+	while ((c = **pos_str)) {
+		(*pos_str)++;
+		switch (c) {
+			case '"':
+				while ((c = **pos_str) && c != '"') {
+					*(*pos_buf)++ = c;
+					(*pos_str)++;
+				}
+				if (!c) {
+					/* error: missing '"' */
+				} else {
+					/* skip closing '"' */
+					(*pos_str)++;
+				}
+				*(*pos_buf)++ = 0;
+				return ret;
+				break;
+			case ' ':
+				*(*pos_buf)++ = 0;
+				return ret;
+				break;
+			case '\\':
+				/* TODO: more escapes here (on 2022-01-09) */
+				switch ((c = *(*pos_str)++)) {
+					case ' ':
+						*(*pos_buf)++ = c;
+						break;
+					default:
+						*(*pos_buf)++ = '\\';
+						*(*pos_buf)++ = c;
+				}
+				break;
+			default:
+				*(*pos_buf)++ = c;
+				break;
+		}
+	}
+	*(*pos_buf)++ = 0;
+	return ret;
+}
--- src/tokenize.h
+++ src/tokenize.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <stdlib.h> // NULL
-
 // Tokenizes a string separated by whitespace. Simple quoting works.
 // On the first call, str and buf may not be NULL. On each consecutive call
 // str must be NULL.
@@ -10,58 +8,4 @@
 //             pos_str, pos_buf: will be initialized on the first call.
 // Returns:    char* to the token inside buf
 //             NULL if there are no more tokens
-char *tokenize(const char *str, char *buf, const char **pos_str, char **pos_buf)
-{
-	char c;
-	if (str) {
-		*pos_str = str;
-		*pos_buf = buf;
-	}
-
-	while ((c = **pos_str) && c == ' ')
-		(*pos_str)++;
-
-	if (!c)
-		return NULL;
-
-	char *ret = *pos_buf;
-	while ((c = **pos_str)) {
-		(*pos_str)++;
-		switch (c) {
-			case '"':
-				while ((c = **pos_str) && c != '"') {
-					*(*pos_buf)++ = c;
-					(*pos_str)++;
-				}
-				if (!c) {
-					/* error: missing '"' */
-				} else {
-					/* skip closing '"' */
-					(*pos_str)++;
-				}
-				*(*pos_buf)++ = 0;
-				return ret;
-				break;
-			case ' ':
-				*(*pos_buf)++ = 0;
-				return ret;
-				break;
-			case '\\':
-				/* TODO: more escapes here (on 2022-01-09) */
-				switch ((c = *(*pos_str)++)) {
-					case ' ':
-						*(*pos_buf)++ = c;
-						break;
-					default:
-						*(*pos_buf)++ = '\\';
-						*(*pos_buf)++ = c;
-				}
-				break;
-			default:
-				*(*pos_buf)++ = c;
-				break;
-		}
-	}
-	*(*pos_buf)++ = 0;
-	return ret;
-}
+char *tokenize(const char *str, char *buf, const char **pos_str, char **pos_buf);
