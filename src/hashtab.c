b47d208043c6c0169697bacef1d276024ac59016
kmarius <froase@gmail.com>
2022-04-04 12:02:13 +0200

    add hashtab.{c,h}

---
+++ src/hashtab.c
@@ -0,0 +1,93 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "hashtab.h"
+#include "util.h"
+
+#define T Hashtab
+
+struct bucket {
+	const char *key;
+	void *val;
+	struct bucket *next;
+};
+
+// good enough for now
+static uint16_t hash(const char *s, uint16_t n)
+{
+	uint64_t h = 0;
+	while (*s)
+		h = *s++ + (h << 6) + (h << 16) - h;
+	return h % n;
+}
+
+
+T *hashtab_init(T *t, size_t size, free_fun free)
+{
+	t->nbuckets = size;
+	t->buckets = calloc(t->nbuckets, sizeof *t->buckets);
+	t->free = free;
+	t->version = 0;
+	return t;
+}
+
+
+void hashtab_deinit(T *t)
+{
+	for (size_t i = 0; i < t->nbuckets; i++) {
+		if (t->buckets[i].val) {
+			for (struct bucket *next, *b = t->buckets[i].next; b; b = next) {
+				next = b->next;
+				if (t->free)
+					t->free(b->val);
+				free(b);
+			}
+			if (t->free)
+				t->free(t->buckets[i].val);
+		}
+	}
+	free(t->buckets);
+}
+
+
+// Returns true if successfull and the corresponding bucket in b: Otherwise,
+// b contains the bucket (head of the list) if empty, or the element to which
+// the new node should be appended to (check with (*b)->val).
+static bool probe(T *t, const char *key, struct bucket **b)
+{
+	uint16_t h = hash(key, t->nbuckets);
+	*b = &t->buckets[h];
+	for (;;) {
+		struct bucket *bb = *b;
+		if (!bb->key)
+			return false;
+		if (streq(bb->key, key))
+			return true;
+		if (!bb->next)
+			return false;
+		*b = bb->next;
+	}
+}
+
+
+void hashtab_set(T *t, const char *key, void *val)
+{
+	struct bucket *b;
+	if (!probe(t, key, &b) && b->val) {
+		b->next = malloc(sizeof *b->next);
+		b = b->next;
+		b->next = NULL;
+	}
+	b->key = key;
+	b->val = val;
+}
+
+
+void *hashtab_get(T *t, const char *key)
+{
+	struct bucket *b;
+	if (probe(t, key, &b))
+		return b->val;
+	return NULL;
+}
---
+++ src/hashtab.h
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include <stdint.h>
+
+// Minimal hash table to be used in directory/preview caches.
+
+struct bucket;
+typedef void (*free_fun)(void *);
+
+typedef struct hashmap {
+	struct bucket *buckets;
+	uint16_t nbuckets;
+	free_fun free;
+	uint8_t version;
+} Hashtab;
+
+Hashtab *hashtab_init(Hashtab *t, size_t size, free_fun free);
+void hashtab_deinit(Hashtab *t);
+void hashtab_set(Hashtab *t, const char *key, void *val);
+void *hashtab_get(Hashtab *t, const char *key);
