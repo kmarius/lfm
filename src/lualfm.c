36a435d490e5a8bb969393b8f5ef35f45d125ce0
kmarius <5224719+kmarius@users.noreply.github.com>
2022-10-16 13:46:36 +0200

    cleanup l_menu

--- src/lualfm.c
+++ src/lualfm.c
@@ -953,25 +953,19 @@ static int l_ui_get_height(lua_State *L)
 static int l_ui_menu(lua_State *L)
 {
   cvector_vector_type(char*) menubuf = NULL;
-  if (lua_type(L, -1) == LUA_TTABLE) {
-    cvector_grow(menubuf, lua_objlen(L, -1));
-    for (lua_pushnil(L); lua_next(L, -2); lua_pop(L, 1)) {
-      cvector_push_back(menubuf, strdup(luaL_checkstring(L, -1)));
+  if (lua_type(L, 1) == LUA_TTABLE) {
+    const int n = lua_objlen(L, 1);
+    for (int i = 1; i <= n; i++) {
+      lua_rawgeti(L, 1, i);
+      cvector_push_back(menubuf, strdup(lua_tostring(L, -1)));
+      lua_pop(L, 1);
     }
   } else if (lua_type(L, -1) == LUA_TSTRING) {
     const char *str = lua_tostring(L, 1);
-    for (;;) {
-      const char *nl = strchr(str, '\n');
-      if (!nl) {
-        cvector_push_back(menubuf, strdup(str));
-        break;
-      }
-      char *line = malloc(nl - str + 1);
-      strncpy(line, str, nl - str);
-      line[nl - str] = 0;
-      cvector_push_back(menubuf, line);
-      str = nl + 1;
+    for (const char *nl; (nl = strchr(str, '\n')); str = nl + 1) {
+      cvector_push_back(menubuf, strndup(str, nl - str));
     }
+    cvector_push_back(menubuf, strdup(str));
   }
   ui_menu_show(ui, menubuf);
   return 0;
