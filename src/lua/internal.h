22641657d96a5dcd1ce6a06aef76bac58b1df2e6
kmarius <5224719+kmarius@users.noreply.github.com>
2023-09-11 16:48:06 +0200

    improve how we retrieve lua callbacks from the registry

--- src/lua/internal.h
+++ src/lua/internal.h
@@ -13,7 +13,8 @@ extern Fm *fm;
 
 #define luaL_optbool(L, i, d) lua_isnoneornil(L, i) ? d : lua_toboolean(L, i)
 
-#define lua_quit(L, lfm) (lfm_quit(lfm), luaL_error(L, "quit"));
+#define lua_quit(L, lfm)                                                       \
+  (lfm_quit(lfm), luaL_error(L, "no actual error, just quiting"));
 
 // Stores the element at the top of the stack in the registry and returns the
 // reference index.
@@ -24,11 +25,11 @@ static inline int lua_set_callback(lua_State *L) {
 
 // Gets the previously stored (via lua_set_callback) element with reference ref
 // from the registry and leaves it at the top of the stack.
-// Returns `true` if the element is a function.
-static inline bool lua_get_callback(lua_State *L, int ref, bool unref) {
+static inline void lua_get_callback(lua_State *L, int ref, bool unref) {
+  assert(ref >= 0);
   lua_rawgeti(L, LUA_REGISTRYINDEX, ref); // [elem]
   if (unref) {
     luaL_unref(L, LUA_REGISTRYINDEX, ref);
   }
-  return lua_type(L, -1) == LUA_TFUNCTION;
+  assert(!lua_isnoneornil(L, -1));
 }
