77c86c7440cb890357a3d67bbca554009a12e7fa
kmarius <5224719+kmarius@users.noreply.github.com>
2023-10-21 18:50:49 +0200

    remove source directory prefix from logs

--- src/log.h
+++ src/log.h
@@ -29,12 +29,16 @@ typedef void (*log_LockFn)(bool lock, void *udata);
 
 enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
-#define log_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
-#define log_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define log_info(...) log_log(LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
-#define log_warn(...) log_log(LOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
-#define log_error(...) log_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
-#define log_fatal(...) log_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
+// SOURCE_PATH_SIZE is passed via the compile to remove the common prefix from
+// __FILE__
+#define __RELFILE__ (&__FILE__[SOURCE_PATH_SIZE])
+
+#define log_trace(...) log_log(LOG_TRACE, __RELFILE__, __LINE__, __VA_ARGS__)
+#define log_debug(...) log_log(LOG_DEBUG, __RELFILE__, __LINE__, __VA_ARGS__)
+#define log_info(...) log_log(LOG_INFO, __RELFILE__, __LINE__, __VA_ARGS__)
+#define log_warn(...) log_log(LOG_WARN, __RELFILE__, __LINE__, __VA_ARGS__)
+#define log_error(...) log_log(LOG_ERROR, __RELFILE__, __LINE__, __VA_ARGS__)
+#define log_fatal(...) log_log(LOG_FATAL, __RELFILE__, __LINE__, __VA_ARGS__)
 
 const char *log_level_string(int level);
 void log_set_lock(log_LockFn fn, void *udata);
