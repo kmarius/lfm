1e237b03a9db05ad41fc9556d2ba3f7cf8db02c4
kmarius <5224719+kmarius@users.noreply.github.com>
2024-08-02 09:33:15 +0200

    docs: filter.h

--- src/filter.h
+++ src/filter.h
@@ -8,12 +8,49 @@
 
 typedef struct Filter Filter;
 
+/*
+ * Creates a filter that matches substrings case insensitively.
+ * `filter` expressions are negated if the string in the pattern begins with
+ * `!`, ` ` (space) works as a logical `and`, `|` as logical `or`.
+ * Additionally, files can be filtered by size with "s>1M", "s<4k" etc.
+ */
 Filter *filter_create_sub(const char *filter);
+
+/*
+ * Creates a filter fuzzy matches against the filter pattern.
+ * Additionally sets the score on a file upon match so that files
+ * can be sorted with the `filter_cmp` compare function.
+ */
 Filter *filter_create_fuzzy(const char *filter);
+
+/*
+ * Creates a filter that calls the lua function with reference `ref` with the
+ * file name. `L` is the lua_State.
+ */
 Filter *filter_create_lua(int ref, void *L);
 
+/*
+ * Destroy a filter object.
+ */
 void filter_destroy(Filter *filter);
+
+/*
+ * Match the filter against a file.
+ */
 bool filter_match(const Filter *filter, const File *file);
+
+/*
+ * Get the pattern string used to create the filter.
+ */
 const char *filter_string(const Filter *filter);
+
+/*
+ * Get a string representing the type of the filter, currently either
+ * "filter", "fuzzy", or "lua".
+ */
 const char *filter_type(const Filter *filter);
+
+/*
+ * Get the compare function of a filter, if it has one. Returns NULL otherwise.
+ */
 __compar_fn_t filter_cmp(const Filter *filter);
