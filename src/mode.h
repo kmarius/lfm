c3146673d9c96be1b1a155ec933a229560124540
kmarius <5224719+kmarius@users.noreply.github.com>
2024-08-02 10:21:06 +0200

    docs: mode.h

--- src/mode.h
+++ src/mode.h
@@ -47,22 +47,56 @@ static inline struct mode mode_valfrom(struct mode mode) {
 #define i_eq(p, q) (!strcmp(*(p), *(q)))
 #include "stc/hmap.h"
 
+/*
+ * Initialize mode functionality.
+ */
 void lfm_modes_init(struct Lfm *lfm);
 
+/*
+ * Deinitialize mode functionality.
+ */
 void lfm_modes_deinit(struct Lfm *lfm);
 
+/*
+ * Register a new mode. Returns 1 if a mode with the same name already exists, 0
+ * otherwise.
+ */
 int lfm_mode_register(struct Lfm *lfm, const struct mode *mode);
 
+/*
+ * Enter the mode with the name `name`. Calls necessary callbacks/hooks
+ * and cleans/sets up the command line.
+ * Returns 1 if the mode does not exist, 0 otherwise.
+ */
 int lfm_mode_enter(struct Lfm *lfm, const char *name);
 
+/*
+ * Exit the mode (i.e. enters "normal" mode) with the name `name` if it is
+ * the current mode. Returns 1 if a different mode is active.
+ */
 int lfm_mode_exit(struct Lfm *lfm, const char *name);
 
+/*
+ * Call on_enter callback for `mode`.
+ */
 void mode_on_enter(struct mode *mode, struct Lfm *lfm);
 
+/*
+ * Call on_change callback for `mode`.
+ */
 void mode_on_change(struct mode *mode, struct Lfm *lfm);
 
+/*
+ * Call on_return callback with command `line` for `mode`.
+ */
 void mode_on_return(struct mode *mode, struct Lfm *lfm, const char *line);
 
+/*
+ * Call on_exit callback for `mode`.
+ */
 void mode_on_exit(struct mode *mode, struct Lfm *lfm);
 
+/*
+ * Call on_escape callback for `mode`.
+ */
 void mode_on_esc(struct mode *mode, struct Lfm *lfm);
