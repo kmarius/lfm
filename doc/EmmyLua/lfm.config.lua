cf7e01c753947eef0d57748f969e5e4f3b622f9d
kmarius <5224719+kmarius@users.noreply.github.com>
2024-09-08 11:11:23 +0200

    docs: typos

--- doc/EmmyLua/lfm.config.lua
+++ doc/EmmyLua/lfm.config.lua
@@ -1,7 +1,7 @@
 ---@meta
 
 ---@class Lfm.DirSetting
----@field sorttype Lfm.SortOption
+---@field sorttype Lfm.SortType
 ---@field dirfirst boolean
 ---@field reverse boolean
 ---@field hidden boolean
@@ -30,12 +30,12 @@
 
 ---@class Lfm.Config
 ---@field timefmt string Time format used to print dates for file info (default: "%Y-%m-%d %H:%M")
----@field loading_indicator_delay integer delay in ms after which an indicator will be shown that the current directory is being reloaded/checked (default: 250)
----@field map_clear_delay integer delay in ms after which the current key input will be cleared, must be non-negative, 0 disables (default: 10000)
----@field map_suggestion_delay integer delay in ms after which key suggestions will be shown, must be non-negative (default: 1000)
----@field histsize integer history size, must be non-negative (default: 100)
----@field infoline string|nil infoline string
----@field threads integer number of threads in the pool (at least 2, default: nprocs+1)
+---@field loading_indicator_delay integer Delay in ms after which an indicator will be shown that the current directory is being reloaded/checked (default: 250)
+---@field map_clear_delay integer Delay in ms after which the current key input will be cleared, must be non-negative, 0 disables (default: 10000)
+---@field map_suggestion_delay integer Delay in ms after which key suggestions will be shown, must be non-negative (default: 1000)
+---@field histsize integer History size, must be non-negative (default: 100)
+---@field infoline string|nil Infoline string
+---@field threads integer Number of threads in the pool (at least 2, default: nprocs+1)
 ---@field dir_settings table<Lfm.Path, Lfm.DirSetting>
 ---@field ratios integer[] assignable
 ---@field truncatechar string assignable, only the first character is used
@@ -58,5 +58,5 @@
 ---@field colors Lfm.Colors
 ---@field inotify_blacklist string[] No inotify watchers will be installed if the path begins with any of these strings.
 ---@field inotify_timeout number Minimum time in milliseconds between reloads triggered by inotify. Must larger or equal to 100.
----@field inotify_delay number Small delay in milliseconds before relloads are triggered by inotify.
+---@field inotify_delay number Small delay in milliseconds before reloads are triggered by inotify.
 lfm.config = {}
