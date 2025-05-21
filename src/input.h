acb0e01def2704ed8161af2a0c5af16a9afdc813
kmarius <5224719+kmarius@users.noreply.github.com>
2025-05-21 21:14:35 +0200

    remove old function declaration

--- src/input.h
+++ src/input.h
@@ -18,9 +18,6 @@ void input_suspend(struct Lfm *lfm);
 
 void input_handle_key(struct Lfm *lfm, input_t in);
 
-// Set input timout. Key input will be ignored for the next `duration` ms.
-void input_timeout_set(struct Lfm *lfm, uint32_t duration);
-
 // Maps a key sequence to a lua function (i.e. a reference to the registry).
 // Pass `ref == 0` to unmap. Returns the previous reference/reference that was
 // removed.
