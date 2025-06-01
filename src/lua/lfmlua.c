b0b2d0edb158f3d738499c7567142b756421ce70
kmarius <5224719+kmarius@users.noreply.github.com>
2025-06-01 11:54:17 +0200

    lua: load lfm.fs directly in thread

--- src/lua/lfmlua.c
+++ src/lua/lfmlua.c
@@ -217,18 +217,6 @@ bool llua_load_file(lua_State *L, const char *path, bool err_on_non_exist) {
 }
 
 void lfm_lua_init_thread(lua_State *L) {
-  lua_getglobal(L, "package");    // [package]
-  lua_getfield(L, -1, "preload"); // [package, preload]
-  for (size_t i = 0; i < ARRAY_SIZE(builtin_modules); i++) {
-    ModuleDef def = builtin_modules[i];
-    if (streq(def.name, "lfm.fs")) {
-      lua_pushinteger(L, (long)i); // [package, preload, i]
-      lua_pushcclosure(L, l_module_preloader,
-                       1);           // [package, preload, cclosure]
-      lua_setfield(L, -2, def.name); // [package, preload]
-    }
-  }
-  lua_pop(L, 2); // []
 
   lua_newtable(L);
 
@@ -237,6 +225,24 @@ void lfm_lua_init_thread(lua_State *L) {
 
   luaopen_fn(L);
   lua_setfield(L, -2, "fn");
+
+  for (size_t i = 0; i < ARRAY_SIZE(builtin_modules); i++) {
+    ModuleDef def = builtin_modules[i];
+    if (streq(def.name, "lfm.fs")) {
+      if (luaL_loadbuffer(L, (const char *)def.data, def.size - 1, def.name)) {
+        log_error("%s", lua_tostring(L, -1));
+        lua_pop(L, 1);
+        return;
+      }
+      if (lua_pcall(L, 0, 1, 0)) {
+        log_error("%s", lua_tostring(L, -1));
+        lua_pop(L, 1);
+        return;
+      }
+      lua_setfield(L, -2, "fs");
+      break;
+    }
+  }
 
   lua_setglobal(L, "lfm");
 
