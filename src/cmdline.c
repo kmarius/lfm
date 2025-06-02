e01ad87b560de56d60a3d0be47c4498e40185ae3
kmarius <5224719+kmarius@users.noreply.github.com>
2025-06-02 17:37:42 +0200

    cmdline cleanup

--- src/cmdline.c
+++ src/cmdline.c
@@ -1,5 +1,4 @@
 #include "cmdline.h"
-#include "history.h"
 
 #define STC_CSTR_UTF8
 #include "stc/cstr.h"
@@ -8,6 +7,7 @@
 #include "stc/zsview.h"
 
 #include "config.h"
+#include "history.h"
 #include "macros.h"
 #include "ncutil.h"
 #include "profiling.h"
@@ -17,119 +17,125 @@
 #include <stdint.h>
 
 #define get_ui(cmdline_) container_of(cmdline_, struct Ui, cmdline)
+#define Self Cmdline
 
-void cmdline_init(Cmdline *c) {
-  c->prefix = cstr_init();
-  c->left = cstr_init();
-  c->right = cstr_init();
-  c->buf = cstr_with_capacity(32);
-  c->overwrite = false;
+void cmdline_init(Self *self) {
+  self->prefix = cstr_init();
+  self->left = cstr_init();
+  self->right = cstr_init();
+  self->buf = cstr_with_capacity(32);
+  self->overwrite = false;
   PROFILE("history_load",
-          { history_load(&c->history, cstr_zv(&cfg.historypath)); })
+          { history_load(&self->history, cstr_zv(&cfg.historypath)); })
 }
 
-void cmdline_deinit(Cmdline *c) {
-  if (c == NULL)
+void cmdline_deinit(Self *self) {
+  if (self == NULL)
     return;
 
-  history_write(&c->history, cstr_zv(&cfg.historypath), cfg.histsize);
-  history_deinit(&c->history);
+  history_write(&self->history, cstr_zv(&cfg.historypath), cfg.histsize);
+  history_deinit(&self->history);
 
   fputs("\033[2 q", stdout);
 
-  cstr_drop(&c->prefix);
-  cstr_drop(&c->buf);
-  cstr_drop(&c->left);
-  cstr_drop(&c->right);
+  cstr_drop(&self->prefix);
+  cstr_drop(&self->buf);
+  cstr_drop(&self->left);
+  cstr_drop(&self->right);
 }
 
-bool cmdline_prefix_set(Cmdline *c, zsview zv) {
-  cstr_assign_zv(&c->prefix, zv);
+bool cmdline_prefix_set(Self *self, zsview zv) {
+  cstr_assign_zv(&self->prefix, zv);
   return true;
 }
 
-bool cmdline_insert(Cmdline *c, zsview zv) {
-  if (cstr_is_empty(&c->prefix))
+bool cmdline_insert(Self *self, zsview zv) {
+  if (cstr_is_empty(&self->prefix))
     return false;
 
   csview sv = zsview_u8_subview(zv, 0, 1);
-  cstr_append_sv(&c->left, sv);
+  cstr_append_sv(&self->left, sv);
 
-  if (c->overwrite && !cstr_is_empty(&c->right)) {
-    cstr_u8_erase(&c->right, 0, 1);
+  if (self->overwrite && !cstr_is_empty(&self->right)) {
+    cstr_u8_erase(&self->right, 0, 1);
   }
   return true;
 }
 
-bool cmdline_toggle_overwrite(Cmdline *c) {
-  c->overwrite = !c->overwrite;
+bool cmdline_toggle_overwrite(Self *self) {
+  self->overwrite = !self->overwrite;
   return true;
 }
 
-bool cmdline_delete(Cmdline *c) {
-  if (cstr_is_empty(&c->prefix) || cstr_is_empty(&c->left))
+bool cmdline_delete(Self *self) {
+  if (cstr_is_empty(&self->prefix) || cstr_is_empty(&self->left))
     return false;
 
-  cstr_pop(&c->left);
+  cstr_pop(&self->left);
   return true;
 }
 
-bool cmdline_delete_right(Cmdline *c) {
-  if (cstr_is_empty(&c->prefix) || cstr_is_empty(&c->right))
+bool cmdline_delete_right(Self *self) {
+  if (cstr_is_empty(&self->prefix) || cstr_is_empty(&self->right))
     return false;
 
-  cstr_u8_erase(&c->right, 0, 1);
+  cstr_u8_erase(&self->right, 0, 1);
   return true;
 }
 
-bool cmdline_delete_word(Cmdline *c) {
-  if (cstr_is_empty(&c->prefix) || cstr_is_empty(&c->left))
+bool cmdline_delete_line_left(Self *self) {
+  if (cstr_is_empty(&self->prefix) || cstr_is_empty(&self->left))
     return false;
 
-  zsview left = cstr_zv(&c->left);
-  int i = left.size;
-  if (i > 0 && ispunct(left.str[i - 1])) {
-    i--;
-  }
-  if (i > 0 && isspace(left.str[i - 1])) {
-    while (i > 0 && isspace(left.str[i - 1])) {
-      i--;
-    }
-  } else {
-    while (i > 0 && !(isspace(left.str[i - 1]) || ispunct(left.str[i - 1]))) {
-      i--;
-    }
-  }
-
-  _cstr_set_size(&c->left, i);
-
-  return true;
-}
-
-bool cmdline_delete_line_left(Cmdline *c) {
-  if (cstr_is_empty(&c->prefix) || cstr_is_empty(&c->left))
-    return false;
-
-  cstr_clear(&c->left);
+  cstr_clear(&self->left);
   return true;
 }
 
 /* pass a ct argument to move over words? */
-bool cmdline_left(Cmdline *c) {
-  if (cstr_is_empty(&c->prefix) || cstr_is_empty(&c->left))
+bool cmdline_left(Self *self) {
+  if (cstr_is_empty(&self->prefix) || cstr_is_empty(&self->left))
     return false;
 
-  zsview tail = cstr_u8_tail(&c->left, 1);
-  cstr_insert_sv(&c->right, 0, zsview_sv(tail));
-  cstr_pop(&c->left);
+  zsview tail = cstr_u8_tail(&self->left, 1);
+  cstr_insert_zv(&self->right, 0, tail);
+  cstr_pop(&self->left);
   return true;
 }
 
-bool cmdline_word_left(Cmdline *c) {
-  if (cstr_is_empty(&c->prefix) || cstr_is_empty(&c->left))
+bool cmdline_right(Self *self) {
+  if (cstr_is_empty(&self->prefix) || cstr_is_empty(&self->right))
     return false;
 
-  zsview left = cstr_zv(&c->left);
+  csview cs = cstr_u8_subview(&self->right, 0, 1);
+  cstr_append_sv(&self->left, cs);
+  cstr_erase(&self->right, 0, cs.size);
+  return true;
+}
+
+bool cmdline_home(Self *self) {
+  if (cstr_is_empty(&self->prefix) || cstr_is_empty(&self->left))
+    return false;
+
+  cstr_append_s(&self->left, self->right);
+  cstr_clear(&self->right);
+  c_swap(&self->left, &self->right);
+  return true;
+}
+
+bool cmdline_end(Self *self) {
+  if (cstr_is_empty(&self->prefix) || cstr_is_empty(&self->right))
+    return false;
+
+  cstr_append_s(&self->left, self->right);
+  cstr_clear(&self->right);
+  return true;
+}
+
+bool cmdline_delete_word(Self *self) {
+  if (cstr_is_empty(&self->prefix) || cstr_is_empty(&self->left))
+    return false;
+
+  zsview left = cstr_zv(&self->left);
   int i = left.size;
   if (i > 0 && ispunct(left.str[i - 1])) {
     i--;
@@ -144,17 +150,41 @@ bool cmdline_word_left(Cmdline *c) {
     }
   }
 
-  cstr_insert_zv(&c->right, 0, zsview_tail(left, left.size - i));
-  _cstr_set_size(&c->left, i);
+  _cstr_set_size(&self->left, i);
 
   return true;
 }
 
-bool cmdline_word_right(Cmdline *c) {
-  if (cstr_is_empty(&c->prefix) || cstr_is_empty(&c->right))
+bool cmdline_word_left(Self *self) {
+  if (cstr_is_empty(&self->prefix) || cstr_is_empty(&self->left))
     return false;
 
-  zsview right = cstr_zv(&c->right);
+  zsview left = cstr_zv(&self->left);
+  int i = left.size;
+  if (i > 0 && ispunct(left.str[i - 1])) {
+    i--;
+  }
+  if (i > 0 && isspace(left.str[i - 1])) {
+    while (i > 0 && isspace(left.str[i - 1])) {
+      i--;
+    }
+  } else {
+    while (i > 0 && !(isspace(left.str[i - 1]) || ispunct(left.str[i - 1]))) {
+      i--;
+    }
+  }
+
+  cstr_insert_zv(&self->right, 0, zsview_tail(left, left.size - i));
+  _cstr_set_size(&self->left, i);
+
+  return true;
+}
+
+bool cmdline_word_right(Self *self) {
+  if (cstr_is_empty(&self->prefix) || cstr_is_empty(&self->right))
+    return false;
+
+  zsview right = cstr_zv(&self->right);
 
   int i = 0;
   if (i < right.size && iswpunct(right.str[i])) {
@@ -171,70 +201,41 @@ bool cmdline_word_right(Cmdline *c) {
     }
   }
 
-  cstr_append_sv(&c->left, cstr_subview(&c->right, 0, i));
-  cstr_erase(&c->right, 0, i);
+  cstr_append_sv(&self->left, cstr_subview(&self->right, 0, i));
+  cstr_erase(&self->right, 0, i);
 
   return true;
 }
 
-bool cmdline_right(Cmdline *c) {
-  if (cstr_is_empty(&c->prefix) || cstr_is_empty(&c->right))
-    return false;
-
-  csview cs = cstr_u8_subview(&c->right, 0, 1);
-  cstr_append_sv(&c->left, cs);
-  cstr_erase(&c->right, 0, cs.size);
-
+bool cmdline_clear(Self *self) {
+  cstr_clear(&self->prefix);
+  cstr_clear(&self->buf);
+  cstr_clear(&self->left);
+  cstr_clear(&self->right);
+  self->overwrite = false;
+  history_reset(&self->history);
+  notcurses_cursor_disable(get_ui(self)->nc);
+  ui_menu_hide(get_ui(self));
+  ui_redraw(get_ui(self), REDRAW_CMDLINE | REDRAW_MENU);
   return true;
 }
 
-bool cmdline_home(Cmdline *c) {
-  if (cstr_is_empty(&c->prefix) || cstr_is_empty(&c->left))
-    return false;
-
-  cstr_insert_sv(&c->right, 0, cstr_sv(&c->left));
-  cstr_clear(&c->left);
-  return true;
-}
-
-bool cmdline_end(Cmdline *c) {
-  if (cstr_is_empty(&c->prefix) || cstr_is_empty(&c->right))
-    return false;
-
-  cstr_append_s(&c->left, c->right);
-  cstr_clear(&c->right);
-  return true;
-}
-
-bool cmdline_clear(Cmdline *c) {
-  cstr_clear(&c->prefix);
-  cstr_clear(&c->buf);
-  cstr_clear(&c->left);
-  cstr_clear(&c->right);
-  c->overwrite = false;
-  history_reset(&c->history);
-  notcurses_cursor_disable(get_ui(c)->nc);
-  ui_menu_hide(get_ui(c));
-  ui_redraw(get_ui(c), REDRAW_CMDLINE | REDRAW_MENU);
-  return true;
-}
-
-bool cmdline_set(Cmdline *c, zsview left, zsview right) {
+bool cmdline_set(Self *self, zsview left, zsview right) {
   if (!zsview_is_empty(left))
-    cstr_assign_zv(&c->left, left);
+    cstr_assign_zv(&self->left, left);
   if (!zsview_is_empty(right))
-    cstr_assign_zv(&c->right, right);
+    cstr_assign_zv(&self->right, right);
   return true;
 }
 
 // copied into the buffer on every call
-zsview cmdline_get(Cmdline *c) {
-  cstr_copy(&c->buf, c->left);
-  cstr_append_s(&c->buf, c->right);
-  return cstr_zv(&c->buf);
+zsview cmdline_get(Self *self) {
+  cstr_copy(&self->buf, self->left);
+  cstr_append_s(&self->buf, self->right);
+  return cstr_zv(&self->buf);
 }
 
-uint32_t cmdline_draw(Cmdline *c, struct ncplane *n) {
+uint32_t cmdline_draw(Self *self, struct ncplane *n) {
   ncplane_erase(n);
   ncplane_set_bg_default(n);
   ncplane_set_fg_default(n);
@@ -245,11 +246,11 @@ uint32_t cmdline_draw(Cmdline *c, struct ncplane *n) {
   ncplane_dim_yx(n, NULL, &ncol);
 
   uint32_t ret = 0;
-  ret += ncplane_put_str_ansi_yx(n, 0, 0, cstr_str(&c->prefix));
+  ret += ncplane_put_str_ansi_yx(n, 0, 0, cstr_str(&self->prefix));
   ncol -= ret;
 
-  unsigned left_len = cstr_u8_size(&c->left);
-  unsigned right_len = cstr_u8_size(&c->right);
+  unsigned left_len = cstr_u8_size(&self->left);
+  unsigned right_len = cstr_u8_size(&self->right);
 
   // scroll some if the line is too long
   if (right_len == 0) {
@@ -261,17 +262,17 @@ uint32_t cmdline_draw(Cmdline *c, struct ncplane *n) {
   }
 
   if (offset > 0) {
-    offset = cstr_u8_to_index(&c->left, offset);
+    offset = cstr_u8_to_index(&self->left, offset);
   } else {
     offset = 0;
   }
 
-  ret += ncplane_putstr(n, cstr_str(&c->left) + +offset);
-  ncplane_putstr(n, cstr_str(&c->right));
+  ret += ncplane_putstr(n, cstr_str(&self->left) + +offset);
+  ncplane_putstr(n, cstr_str(&self->right));
 
-  fputs(right_len == 0 ? "\033[2 q"
-        : c->overwrite ? "\033[4 q"
-                       : "\033[6 q",
+  fputs(right_len == 0    ? "\033[2 q"
+        : self->overwrite ? "\033[4 q"
+                          : "\033[6 q",
         stdout); // block/bar cursor
 
   return ret;
--- src/cmdline.h
+++ src/cmdline.h
@@ -15,66 +15,70 @@ typedef struct Cmdline {
   History history;
 } Cmdline;
 
+#define Self Cmdline
+
 // Initialize cmdline.
-void cmdline_init(Cmdline *t);
+void cmdline_init(Self *self);
 
 // Free resources allocated by cmdline.
-void cmdline_deinit(Cmdline *t);
+void cmdline_deinit(Self *self);
 
 // Set the `prefix` of the cmdline.
-bool cmdline_prefix_set(Cmdline *t, zsview zv);
+bool cmdline_prefix_set(Self *self, zsview zv);
 
 // Insert the first multibyte char encountered in `key`. Returns `true` if a
 // redraw is necessary.
-bool cmdline_insert(Cmdline *t, zsview zv);
+bool cmdline_insert(Self *self, zsview zv);
 
 // Toggle insert/overwrite.
-bool cmdline_toggle_overwrite(Cmdline *t);
+bool cmdline_toggle_overwrite(Self *self);
 
 // Delete the char left of the cursor. Returns `true` if a redraw is necessary.
-bool cmdline_delete(Cmdline *t);
+bool cmdline_delete(Self *self);
 
 // Delete the char right of the cursor. Returns `true` if a redraw is necessary.
-bool cmdline_delete_right(Cmdline *t);
+bool cmdline_delete_right(Self *self);
 
 // Delete the word left of the cursor. Returns `true` if a redraw is necessary.
-bool cmdline_delete_word(Cmdline *t);
+bool cmdline_delete_word(Self *self);
 
 // Delete from the beginning of the line to cursor. Returns `true` if a redraw
 // is necessary.
-bool cmdline_delete_line_left(Cmdline *t);
+bool cmdline_delete_line_left(Self *self);
 
 // Move the cursor to the left. Returns `true` if a redraw is necessary.
-bool cmdline_left(Cmdline *t);
+bool cmdline_left(Self *self);
 
 // Move the cursor to the right. Returns `true` if a redraw is necessary.
-bool cmdline_right(Cmdline *t);
+bool cmdline_right(Self *self);
 
 // Move the cursor one word left. Returns `true` if a redraw is necessary.
-bool cmdline_word_left(Cmdline *t);
+bool cmdline_word_left(Self *self);
 
 // Move the cursor one word right. Returns `true` if a redraw is necessary.
-bool cmdline_word_right(Cmdline *t);
+bool cmdline_word_right(Self *self);
 
 // Move the cursor to the beginning of the line. Returns `true` if a redraw is
 // necessary.
-bool cmdline_home(Cmdline *t);
+bool cmdline_home(Self *self);
 
 // Move the cursor to the end of the line. Returns `true` if a redraw is
 // necessary.
-bool cmdline_end(Cmdline *t);
+bool cmdline_end(Self *self);
 
 // Clear the command line and remove the prefix. Returns `true` if a redraw is
 // necessary.
-bool cmdline_clear(Cmdline *t);
+bool cmdline_clear(Self *self);
 
 // Set the command line. `left` and `right` are the strings left and right of
-// the cursor, respectively. NULL is accepted.
-bool cmdline_set(Cmdline *t, zsview left, zsview right);
+// the cursor, respectively.
+bool cmdline_set(Self *self, zsview left, zsview right);
 
-// Returns the currend command line.
-zsview cmdline_get(Cmdline *t);
+// Returns the current, complete command line.
+zsview cmdline_get(Self *self);
 
 // Draw the command line into an ncplane. Returns the number of printed
 // characters of prefix, left so that the cursor can be positioned.
-uint32_t cmdline_draw(Cmdline *t, struct ncplane *n);
+uint32_t cmdline_draw(Self *self, struct ncplane *n);
+
+#undef Self
