9272647c88da6f3d9d49bcd63946f9f307dfa230
kmarius <5224719+kmarius@users.noreply.github.com>
2026-02-21 11:53:03 +0100

    lua: add get_list, jump_to to lfm.jumplist

--- runtime/lua/lfm/jumplist.lua
+++ runtime/lua/lfm/jumplist.lua
@@ -8,7 +8,13 @@ local chdir = lfm.api.chdir
 local list = { getpwd() }
 local ind = 1
 
+local disable_hook = false
+
 local function on_chdir(dir)
+	if disable_hook then
+		disable_hook = false
+		return
+	end
 	if list[ind] ~= dir then
 		ind = ind + 1
 		if ind <= #list then
@@ -19,6 +25,41 @@ local function on_chdir(dir)
 			end
 		end
 		list[ind] = dir
+	end
+end
+
+---
+---Get the current jump list and current index.
+---
+---Example:
+---```lua
+---  local list, ind = lfm.jumplist.get_list()
+---```
+---
+---@return table
+---@return integer
+function M.get_list()
+	local ls = {}
+	for i, loc in ipairs(list) do
+		ls[i] = loc
+	end
+	return ls, ind
+end
+
+---
+---Jump to a location in the jump list, by index.
+---
+---Example:
+---```lua
+---  lfm.jumplist.jump_to(7)
+---```
+---
+---@param idx integer
+function M.jump_to(idx)
+	if idx >= 1 and idx <= #list then
+		ind = idx
+		disable_hook = true
+		chdir(list[ind])
 	end
 end
 
